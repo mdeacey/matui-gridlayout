80f098a9e1e28bce822506e30c52d89c417945c3
mdeacey <marcusdeacey@gmail.com>
2015-11-22 17:13:41 +1100

    Updates

--- both/components/home/home.jsx
+++ both/components/home/home.jsx
@@ -1,95 +1,14 @@
-injectTapEventPlugin();
-
-let {
-  FormsyCheckbox,
-  FormsyDate,
-  FormsyRadio,
-  FormsyRadioGroup,
-  FormsySelect,
-  FormsyText,
-  FormsyTime,
-  FormsyToggle
-} = FMUI;
-
-var {
-  Avatar
-} = MUI;
-
 Home = React.createClass({
-  childContextTypes: {
-    muiTheme: React.PropTypes.object
-  },
-
-  getChildContext(){
-    return {
-      muiTheme: Styles.ThemeManager.getMuiTheme(Styles.LightRawTheme)
-    }
-  },
-
-  getInitialState: function () {
-    return {
-      canSumbit: false
-    };
-  },
-
-  errorMessages: {
-    wordsError: "Please only use letters",
-    numericError: "Please provide a number",
-    urlError: "Please provide a valid URL"
-  },
-
-  selectFieldItems: [
-    { payload: 'never', text: 'Never' },
-    { payload: 'nightly', text: 'Every Night' },
-    { payload: 'weeknights', text: 'Weeknights' },
-    { payload: 'weekends', text: 'Weekends' },
-    { payload: 'weekly', text: 'Weekly' }
-  ],
-
-  styles: {
-    paperStyle: {
-      width: 300,
-      margin: 20,
-      padding: 20
-    },
-    switchStyle: {
-      marginBottom:16
-    },
-    submitStyle: {
-      marginTop: 32
-    }
-  },
-
-  enableButton: function () {
-    this.setState({
-      canSubmit: true
-    });
-  },
-
-  disableButton: function () {
-    this.setState({
-      canSubmit: false
-    });
-  },
-
-  submitForm: function (data) {
-    alert(JSON.stringify(data, null, 4));
-  },
-
-  notifyFormError: function (data) {
-    console.error('Form error:', data);
-  },
-
-  render: function () {
-    let {paperStyle, switchStyle, submitStyle } = this.styles;
-    let { wordsError, numericError, urlError } = this.errorMessages;
-
+  render() {
 
     return (
-      <div>
-        This is react-grid form.
-
+      <div className="rgl-wrapper">
+        <ReactGridLayout className="layout" cols={12} rowHeight={30}>
+          <div key={1} _grid={{x: 0, y: 0, w: 1, h: 2}}>1</div>
+          <div key={2} _grid={{x: 1, y: 0, w: 1, h: 2}}>2</div>
+          <div key={3} _grid={{x: 2, y: 0, w: 1, h: 2}}>3</div>
+        </ReactGridLayout>
       </div>
-    );
+    )
   }
 });
